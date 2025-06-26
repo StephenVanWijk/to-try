d68a0b2077b1394e36c530bd1299f28a13cc8681
StephenVanWijk <stephenvanwijk6@gmail.com>
2025-06-26 18:29:41 +0200

    Interesting, Rust can handle C like enums.

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,25 @@
-// Define a Book struct with a lifetime-annotated reference
-// Book struct lifetime-annotated
-struct Book<'a> {
-    title: &'a str,
-    author: &'a str,
-    excerpt: &'a str,
+// An attribute to hide warnings for unused code.
+#![allow(dead_code)]
+
+// enum with implicit discriminator (starts at 0)
+enum Number {
+    Zero,
+    One,
+    Two,
 }
 
-// 20250625 1803 CET SDvW If you keep it like this, most of the cases it must fit.
-// DateDPE seems to work for this.
-impl<'a> Book<'a> {
-    // Constructor method
-    fn new(title: &'a str, author: &'a str, excerpt: &'a str) -> Self {
-        Book { title, author, excerpt }
-    }
-    
-    // Method that returns a reference tied to the same lifetime
-    fn get_title(&self) -> &'a str {
-        self.title
-    }
+// enum with explicit discriminator
+enum Color {
+    Red = 0xff0000,
+    Green = 0x00ff00,
+    Blue = 0x0000ff,
 }
 
 fn main() {
-    // The owned strings must live longer than the Book
-    let title = String::from("The Rust Programming Language");
-    let author = String::from("Steve Klabnik and Carol Nichols");
-    let excerpt = String::from("Welcome to The Rust Programming Language...");
-    
-    // Create a book with references to our strings
-    let book = Book::new(&title, &author, &excerpt);
-    
-    println!(
-        "Book: '{}' by {}\nExcerpt: {}",
-        book.get_title(),
-        book.author,
-        book.excerpt
-    );
-    
-    // All references remain valid because:
-    // 1. The strings (title, author, excerpt) live until the end of main()
-    // 2. The Book's references don't outlive their source data
+    // `enums` can be cast as integers.
+    println!("zero is {}", Number::Zero as i32);
+    println!("one is {}", Number::One as i32);
+
+    println!("roses are #{:06x}", Color::Red as i32);
+    println!("violets are #{:06x}", Color::Blue as i32);
 }
